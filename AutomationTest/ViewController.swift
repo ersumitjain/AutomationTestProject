8d8dcaa73f8dd35a174c66caf3256985017bcef8
Sumit Jain <sumitjain@SumitJain.local>
2018-10-23 16:10:44 +0530

    Initial Commit

---
+++ AutomationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutomationTest
+//
+//  Created by Sumit Jain on 23/10/18.
+//  Copyright © 2018 Sumit Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
